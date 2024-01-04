85865adf65ff8b9a64c8ff8b44db82f303a27b48
BIDcolonel <ptitofdu89@live.fr>
2024-01-04 13:30:37 +0100

    add descrition for the function

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -7,7 +7,7 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Returns a list of delays in ascending order"""
+    """Returns a list of delays in ascending order of float values"""
     delays = []
     for _ in range(n):
         delays.append(await task_wait_random(max_delay))
