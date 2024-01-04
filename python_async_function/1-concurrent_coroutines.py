dbbeeb7af8d41d38dc1f3ba843f9fd41d194b1a7
BIDcolonel <ptitofdu89@live.fr>
2024-01-04 13:29:09 +0100

    add description for the function

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -6,7 +6,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Return the list of all the delays"""
+    """Return the list of all the delays in ascending order"""
     delays = []
     tasks = []
     for _ in range(n):
