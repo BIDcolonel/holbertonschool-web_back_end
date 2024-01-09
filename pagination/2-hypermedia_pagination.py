9922aa365756a6a370272f9c0bce9b1a2a5ff7e7
BIDcolonel <ptitofdu89@live.fr>
2024-01-09 13:16:44 +0100

    Task 2: Replicate code from the previous task.
    Implement a get_hyper method that takes the same arguments (and defaults) as get_page and returns a dictionary containing the following key-value pairs:
    
    page_size: the length of the returned dataset page
    page: the current page number
    data: the dataset page (equivalent to return from previous task)
    next_page: number of the next page, None if no next page
    prev_page: number of the previous page, None if no previous page
    total_pages: the total number of pages in the dataset as an integer
    Make sure to reuse get_page in your implementation.

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+"""Hypermedia pagination module"""
+import csv
+import math
+from typing import List
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """Returns the start and end indices corresponding to a given page and
+    page size.
+
+    Args:
+    - page (int): The page number (1-indexed).
+    - page_size (int): The number of items per page.
+
+    Returns:
+    - tuple[int, int]: A tuple containing the start and end indices for the
+    given page and page size."""
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+
+    return start_index, end_index
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Method that stores the dataset in a list instance attribute named"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Method that takes two integer arguments page with default value 1"""
+        self.dataset()
+        for i in [page, page_size]:
+            assert isinstance(i, int) and page > 0
+        assert page_size > 0
+        range_i = index_range(page, page_size)
+        return self.__dataset[range_i[0]:range_i[1]]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """Get hypermedia pagination.
+
+            Args:
+            - page: page number.
+            - page_size: number of items per page.
+
+            Return:
+            - A dictionary."""
+        data: int = self.get_page(page, page_size)
+        totalPages: int = math.ceil(len(self.__dataset) / page_size)
+        return {
+            'page_size': len(data),
+            'page': page,
+            'data': data,
+            'next_page': page + 1 if (page + 1) <= totalPages else None,
+            'prev_page': page - 1 if (page - 1) > 0 else None,
+            'total_pages': totalPages
+        }
