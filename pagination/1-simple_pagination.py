063fed33df36c1d6974a5935312af5ace71c0a38
BIDcolonel <ptitofdu89@live.fr>
2024-01-09 11:33:53 +0100

    Task 1: Implement a method named get_page that takes two integer arguments page with default value 1 and page_size with default value 10.
    You have to use this CSV file (same as the one presented at the top of the project)
    Use assert to verify that both arguments are integers greater than 0.
    Use index_range to find the correct indexes to paginate the dataset correctly and return the appropriate page of the dataset (i.e. the correct list of rows).
    If the input arguments are out of range for the dataset, an empty list should be returned.

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""Simple pagination module"""
+import csv
+import math
+from typing import List
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """Returns the start and end indices corresponding to a given page and
+    page size.
+
+    Args:
+    - page (int): The page number (1-indexed).
+    - page_size (int): The number of items per page.
+
+    Returns:
+    - tuple[int, int]: A tuple containing the start and end indices for the
+    given page and page size."""
+    if page <= 0 or page_size <= 0:
+        raise ValueError("Page and page_size must be positive integers.")
+
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+
+    return start_index, end_index
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Method that stores the dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Method that takes two integer arguments page with default value 1"""
+        self.dataset()
+        for i in [page, page_size]:
+            assert isinstance(i, int) and page > 0
+        assert page_size > 0
+        range_i = index_range(page, page_size)
+        return self.__dataset[range_i[0]:range_i[1]]
