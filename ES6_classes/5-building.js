fd01912f60641e2c2ff5e705613ac9f334a47d52
BIDcolonel <ptitofdu89@live.fr>
2024-01-17 14:25:26 +0100

    import Building from './5-building.js';
    const b = new Building(100);
    console.log(b);
    
    class TestBuilding extends Building {}
    
    try {
      new TestBuilding(200).evacuationWarningMessage(); // Appeler la méthode pour déclencher l'erreur
    } catch (err) {
      console.log(err.message);
    }

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,22 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+
+  evacuationWarningMessage() {
+    if (this.constructor === Building) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+}
